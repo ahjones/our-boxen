190097ed091c3dd7466415a2b821937282825cf3
Andrew Jones <me@andrewhjon.es>
2015-04-28 21:32:34 +0100

    OS X settings

--- modules/people/manifests/ahjones.pp
+++ modules/people/manifests/ahjones.pp
@@ -5,4 +5,12 @@ class people::ahjones {
   }
 
   service {["dev.nginx", "dev.dnsmasq"]: ensure => stopped}
+
+  include osx::global::enable_standard_function_keys
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+  include osx::dock::clear_dock
+  include osx::finder::show_all_filename_extensions
+  include osx::disable_app_quarantine
+  include osx::keyboard::capslock_to_control
 }
